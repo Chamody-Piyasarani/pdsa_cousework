f314b829b5dbb403e388eae3bbebe5218eb97b13
Senuki-Rathnayake <COHNDSE232F-101@student.nibm.lk>
2024-02-23 23:01:50 +0530

    addCustomer

--- src/Main.java
+++ src/Main.java
@@ -60,110 +60,128 @@ class VehicleNode{     //Define class to contain Vehicle attributes into a node
     }
 
 }
-class RentalManagement{
-    VehicleNode head=null;
-    VehicleNode back=null;
-    CustomerNode cus_head=null;
-    int totalVehicles=0;
-    int totalCusomers=0;
-    Scanner sc=new Scanner(System.in);
+class RentalManagement {
+    VehicleNode head = null;
+    VehicleNode back = null;
+    CustomerNode cus_head = null;
+    int totalVehicles = 0;
+    int totalCusomers = 0;
+    Scanner sc = new Scanner(System.in);
 
-    public RentalManagement(){
-        menu();
+    public RentalManagement() {
+
     }
 
-    public boolean isVehicleExisist(String vehicleID){ // Method to check whether the vehicle id is exist or not
+    public boolean isVehicleExisist(String vehicleID) { // Method to check whether the vehicle id is exist or not
         VehicleNode current = head;
-        while(current!=null){
-            if(current.vehicle.vehicle_id.equals(vehicleID)){
+        while (current != null) {
+            if (current.vehicle.vehicle_id.equals(vehicleID)) {
                 return true;
             }
-            current=current.next;
+            current = current.next;
         }
         return false;
     }
 
-    public boolean isCustomerExsist(String customerID){  // Method to check whether the customer id is exist or not
-        CustomerNode current=cus_head;
-        while(current!=null){
-            if(current.cus.customerId.equals(customerID)){
+    public boolean isCustomerExsist(String customerID) {  // Method to check whether the customer id is exist or not
+        CustomerNode current = cus_head;
+        while (current != null) {
+            if (current.cus.customerId.equals(customerID)) {
                 return true;
             }
         }
         return false;
     }
 
-    public void addVehicle(String vehicle_id,String brand, String model,String type,double chargePerDay){
-        Vehicle newVehicle=new Vehicle(vehicle_id, brand, model, type, chargePerDay);
-        VehicleNode newVehicleNode=new VehicleNode(newVehicle);
+    public void addVehicle(String vehicle_id, String brand, String model, String type, double chargePerDay) {
+        Vehicle newVehicle = new Vehicle(vehicle_id, brand, model, type, chargePerDay);
+        VehicleNode newVehicleNode = new VehicleNode(newVehicle);
 
-        if(isVehicleExisist(vehicle_id)){
+        if (isVehicleExisist(vehicle_id)) {
             System.err.println("This Vehicle is Already Exist");
             return;
         }
 
-        if(head==null){
-            head=newVehicleNode;
-            back=newVehicleNode;
-        }
-        else{
-            back.next=newVehicleNode;
-            newVehicleNode.prev=back;
-            back=newVehicleNode;
+        if (head == null) {
+            head = newVehicleNode;
+            back = newVehicleNode;
+        } else {
+            back.next = newVehicleNode;
+            newVehicleNode.prev = back;
+            back = newVehicleNode;
         }
         totalVehicles++;
     }
 
-    public void deleteVehicle(String vehicleID){
-        VehicleNode current=head;
+    public void deleteVehicle(String vehicleID) {
+        VehicleNode current = head;
 
-        if(!isVehicleExisist(vehicleID)){
+        if (!isVehicleExisist(vehicleID)) {
             System.err.println("Vehicle does not exisist");
             return;
         }
-        while(current!=null){
-            if(current.vehicle.vehicle_id.equals(vehicleID)){
-                if(current.prev!=null){
-                    current=current.next;
-                }
-                else{
-                    head=current.next;
+        while (current != null) {
+            if (current.vehicle.vehicle_id.equals(vehicleID)) {
+                if (current.prev != null) {
+                    current = current.next;
+                } else {
+                    head = current.next;
                 }
 
-                if(current.next!=null){
-                    current=current.prev;
-                }
-                else{
-                    back=current.prev;
+                if (current.next != null) {
+                    current = current.prev;
+                } else {
+                    back = current.prev;
                 }
                 totalVehicles--;
                 return;
             }
-            current=current.next;
+            current = current.next;
         }
     }
 
-    public void editVehicle(String vehicleId, String brand,String model, String type,double chargePerDay){
-        VehicleNode current=head;
+    public void editVehicle(String vehicleId, String brand, String model, String type, double chargePerDay) {
+        VehicleNode current = head;
 
-        if(!isVehicleExisist(vehicleId)){
+        if (!isVehicleExisist(vehicleId)) {
             System.err.println("Vehicle does not exist");
             return;
         }
 
-        while(current!=null){
-            if(current.vehicle.vehicle_id.equals(vehicleId)){
-                current.vehicle.vehicle_brand=brand;
-                current.vehicle.vehicle_model=model;
-                current.vehicle.vehicle_type=type;
-                current.vehicle.chargePerDay=chargePerDay;
+        while (current != null) {
+            if (current.vehicle.vehicle_id.equals(vehicleId)) {
+                current.vehicle.vehicle_brand = brand;
+                current.vehicle.vehicle_model = model;
+                current.vehicle.vehicle_type = type;
+                current.vehicle.chargePerDay = chargePerDay;
                 System.out.println("Vehicle Edited Successfully!");
                 System.out.println("------------------------------");
                 break;
             }
-            current=current.next;
+            current = current.next;
         }
     }
+
+    public void add_customer(String cusID, String cusName, String email, String phone) {
+        if (isCustomerExsist(cusID)) {
+            System.err.println("This ID : " + cusID + " already exist.");
+            return;
+        }
+
+        Customer newCus = new Customer(cusID, cusName, email, phone);
+        CustomerNode cusNode = new CustomerNode(newCus);
+        if (cus_head == null) {
+            cus_head = cusNode;
+        } else {
+            CustomerNode current = cus_head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = cusNode;
+        }
+        totalCusomers++;
+    }
+}
 public class Main {
     public static void main(String[] args) {
 
