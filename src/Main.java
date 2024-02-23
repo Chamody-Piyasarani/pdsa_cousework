e5de95070dfda012c905b47840cc03312886a57b
Chamodya-Piyasarani <COHNDSE232F-047@student.nibm.lk>
2024-02-23 22:29:11 +0530

    commit_2

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,30 @@ class Vehicle{  //define Class for vehicle to declare its attributes
     }
 
 }
+class Customer{ //define Class for Customer to declare its attributes
+    String customerId;
+    String cus_name;
+    String cus_email;
+    String telephone;
+
+    public Customer(String cus_id, String cus_name,String cus_email,String cus_tp){ //Parameterized constructor for pass the values
+        this.customerId=cus_id;
+        this.cus_name=cus_name;
+        this.cus_email=cus_email;
+        this.telephone=cus_tp;
+    }
+}
+
+class CustomerNode{ //Define class to contain Customer attributes into a node
+    Customer cus;
+    CustomerNode next;
+
+    public CustomerNode(Customer cus){ //Paramiterized constructor to pass Customer Object
+        this.cus=cus;
+        this.next=null;
+    }
+
+}
 public class Main {
     public static void main(String[] args) {
 
